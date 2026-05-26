82431ca947fd783d1b4f95f89137bcb44fb69370
sufal kc <kcsufal1@gmail.com>
2026-05-27 00:21:40 +0545

    arithmetic

--- Linux/arithmetic.sh
+++ Linux/arithmetic.sh
@@ -1,1 +1,3 @@
 # A mathematical expression containing +,-,*,^, / and parenthesis will be provided. Read in the expression, then evaluate it. Display the result rounded to decimal places.
+read -r airth
+printf "%.3f\n" "$(echo "$airth" | bc -l)"
